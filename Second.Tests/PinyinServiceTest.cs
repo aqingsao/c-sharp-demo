9664744bc72e50ca879fcc15730c966455e6a420
aqingsao <vinci.zhang@gmail.com>
2013-11-03 07:28:06 -0600

    refactor a bit

--- Second.Tests/PinyinServiceTest.cs
+++ Second.Tests/PinyinServiceTest.cs
@@ -15,25 +15,13 @@ namespace Second.Tests
         public void SetUp()
         {
             _mock = new Mock<IPinyinRepository>();
-            _mock.Setup(pinyinProperty => pinyinProperty.GetPinyin("章")).Returns("zhang");
-            _mock.Setup(pinyinProperty => pinyinProperty.GetPinyin("子")).Returns("zi");
-            _mock.Setup(pinyinProperty => pinyinProperty.GetPinyin("怡")).Returns("yi");
-            _mock.Setup(pinyinProperty => pinyinProperty.GetPinyin("武")).Returns("wu");
-            _mock.Setup(pinyinProperty => pinyinProperty.GetPinyin("曌")).Returns((string) null);
+            _mock.Setup(pinyinRepository => pinyinRepository.GetPinyin("章")).Returns("zhang");
+            _mock.Setup(pinyinRepository => pinyinRepository.GetPinyin("子")).Returns("zi");
+            _mock.Setup(pinyinRepository => pinyinRepository.GetPinyin("怡")).Returns("yi");
+            _mock.Setup(pinyinRepository => pinyinRepository.GetPinyin("武")).Returns("wu");
+            _mock.Setup(pinyinRepository => pinyinRepository.GetPinyin("曌")).Returns((string)null);
 
             _pinyin = new PinyinService(_mock.Object);
-        }
-
-        [Test]
-        public void should_return_Zhang_when_given_章()
-        {
-            Assert.That(_pinyin.GetPinyin("章"), Is.EqualTo("Zhang"));
-        }
-
-        [Test]
-        public void should_return_Zi_when_given_子()
-        {
-            Assert.That(_pinyin.GetPinyin("子"), Is.EqualTo("Zi"));
         }
 
         [Test]
@@ -42,20 +30,30 @@ namespace Second.Tests
             Assert.That(_pinyin.GetPinyin("章子怡"), Is.EqualTo("ZhangZiYi"));
         }
 
+        [Test]
+        public void should_return_ZZY_when_given_章子怡()
+        {
+            Assert.That(_pinyin.GetPinyinHeader("章子怡"), Is.EqualTo("ZZY"));
+        }
+
+        [Test()]
+        public void should_return_WQuestionMark_when_given_武笊()
+        {
+            Assert.That(_pinyin.GetPinyinHeader("武曌"), Is.EqualTo("W?"));
+        }
+
         [Test()]
         public void should_return_WuQuestionMark_when_given_武笊()
         {
-            Assert.That(_pinyin.GetPinyin("武笊"), Is.EqualTo("Wu?"));
+            Assert.That(_pinyin.GetPinyin("武曌"), Is.EqualTo("Wu?"));
         }
 
         [Test]
-        [NUnit.Framework.ExpectedException(typeof(Exception))]
         public void should_update_pinyin_of_朝_to_chao()
         {
-            _mock.Setup(pinyinProperty => pinyinProperty.UpdatePinyin("朝", "chao")).Throws(new Exception());
             _pinyin.UpdatePinyin("朝", "chao");
 
-            _mock.Verify(pinyinProperty=>pinyinProperty.UpdatePinyin("朝", "chao"));
+            _mock.Verify(pinyinProperty=>pinyinProperty.UpdatePinyin("朝", "chao"), Times.Once());
         }
     }
 }
