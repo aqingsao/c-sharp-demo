585712e20a389646e37605929bfa6bb47f0d7832
aqingsao <vinci.zhang@gmail.com>
2013-11-07 07:21:56 -0600

    move StringParser in a separate file

---
+++ First/StringParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace First
+{
+    public class StringParser
+    {
+        public int ParseAndSum(string str)
+        {
+            if (String.IsNullOrEmpty(str))
+            {
+                return 0;
+            }
+
+            int total = 0;
+            string[] strings = str.Split(',');
+            for (int i = 0; i < strings.Length; i++)
+            {
+                total += Int32.Parse(strings[i]);
+            }
+            return total;
+        }
+    }
+}
